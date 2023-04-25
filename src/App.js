4b483aea94c11182b50299d9811538a7367a99b9
J.M <kabugijeff@gmail.com>
2023-04-25 07:47:59 +0300

    completed assignment

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React, { useState, useEffect } from "react";
 
 function App() {
-  useEffect(() => {
-    console.log("useEffect called");
-  });
+  const [count, setCount] = useState(1);
+  const [text, setText] = useState("");
 
+  useEffect(() => {
+    document.title = text;
+  }, [text]);
+
+  useEffect(() => {
+    setTimeout(() => setCount(0), 5000);
+    console.log("after")
+  }, [])
+
+  
   console.log("Component rendering");
 
-  return <button>Click Me</button>;
+  return (
+    <div>
+      <button onClick={() => setCount(count => count+1)}>Click Me {count}</button>
+      <input type="text" value={text} placeholder="Type away ..." onChange={(e) => setText(e.target.value)} />
+    </div>
+  )
 }
 
 export default App;
